5a283bc7d56acd817d770adf4d8c4a6ee3eb0b9a
Ivan Clint A. Pabelona <ipabelona@gmail.com>
2020-06-19 19:06:34 +0800

    search form identifiers
    Signed-off-by: Ivan Clint A. Pabelona <ipabelona@gmail.com>

--- views/partials/search.blade.php
+++ views/partials/search.blade.php
@@ -1,7 +1,7 @@
-<form action="{{ Forum::route('index') }}" method="GET">
+<form class="search-form" action="{{ Forum::route('index') }}" method="GET">
     <div class="row mb-3">
         <div class="col-sm-12">
-            <input type="text" class="form-control" placeholder="Search..." name="q" value="{{ request('q') }}">
+            <input type="text" class="form-control search-field" placeholder="Search..." name="q" value="{{ request('q') }}">
         </div>
     </div>
 </form>
