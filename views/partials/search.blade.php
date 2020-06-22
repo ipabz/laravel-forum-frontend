c26ed0db4bd82e5241164617998a95dd2ca71001
Ivan Clint A. Pabelona <ipabelona@gmail.com>
2020-06-22 21:02:21 +0800

    clear search
    Signed-off-by: Ivan Clint A. Pabelona <ipabelona@gmail.com>

--- views/partials/search.blade.php
+++ views/partials/search.blade.php
@@ -4,7 +4,10 @@
             <div class="input-group">
                 <input type="text" class="form-control search-field" placeholder="Search..." name="q" value="{{ request('q') }}">
                 <div class="input-group-append">
-                    <button class="btn btn-primary search-button" type="button">Search</button>
+                    <button class="btn btn-primary search-button" type="button" style="border-bottom-right-radius: 4px; border-top-right-radius: 4px">Search</button>
+                </div>
+                <div class="input-group-append">
+                    <a class="btn btn-link" href="{{ \Illuminate\Support\Facades\URL::current() }}">Clear Search</a>
                 </div>
             </div>
         </div>
